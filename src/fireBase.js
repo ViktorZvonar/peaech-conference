90a12528fcb44175bb12c58fc28b4d83338a9471
ViktorZvonar <viktorzvonar@yahoo.com>
2023-08-14 17:29:02 +0200

    evp edited

--- src/fireBase.js
+++ src/fireBase.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAmVost9Fm8M7Z0V3Rv6wHAz0yc2TBel3c',
-  authDomain: 'peaech-93600.firebaseapp.com',
-  projectId: 'peaech-93600',
-  storageBucket: 'peaech-93600.appspot.com',
-  messagingSenderId: '707957154036',
-  appId: '1:707957154036:web:e394fa2dcfa5a03a023ea1',
-  measurementId: 'G-7TJ84QH0J6',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
